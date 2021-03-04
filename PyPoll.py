6a42d59e34316501d8811882ad735dd9c7a52abd
Alan Goldberg <alan.goldberg@yahoo.com>
2021-03-03 23:57:39 -0500

    update PyPoll: include candidate total and percentages

--- PyPoll.py
+++ PyPoll.py
@@ -4,31 +4,42 @@ import csv
 
 # opening files
 
-# file to load - the module calls thi direct path
-# file_to_load = 'resources/election_results.csv'
 # make it os independent 
+# input file
 file_to_load = os.path.join("resources","election_results.csv")
 # file we are writing results to
 file_to_save = os.path.join("analysis","election_analysis.txt")
 
+# initialize the counter, candidate list, votes dictionary
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+
 # open the input file
-election_data = open(file_to_load,'r')
-# open the output file
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
-    # skip dat header!
-    next(file_reader)
+    # get the header
+    headers = next(file_reader)
     for row in file_reader:
-       print(row)
-        # print the 1st item on each row
-        # print(row[0])
+       total_votes += 1
+       candidate_name = row[2]
+       # get unique candidate names and set their vote count to zero
+       if candidate_name not in candidate_options:
+           candidate_options.append(candidate_name)
+           candidate_votes[candidate_name] = 0
+        
+       candidate_votes[candidate_name] += 1
 
-#with open(file_to_save,'w') as txt_file:
+#for thief in candidate_options:
+for thief in candidate_votes:
+    print (f"{thief}: received {(candidate_votes[thief] / total_votes):.1%} of the vote.")  
 
+# print(total_votes)
+# print(candidate_options)
+# print(candidate_votes)
+# with open(file_to_save,'w') as txt_file:
 # some practice code from before...
     # write some data
     # txt_file.write("Hello World")
     # txt_file.write("Counties in the Election\n------------------------\n")
     # txt_file.write("Arapahoe\nDenver\nJefferson")
-    # to do: Code to perform analysis
-    # print (election_results)
